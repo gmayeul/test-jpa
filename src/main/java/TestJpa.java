f6859e532fdd14eeeb5681990b58b746f545aa0f
Mayeul Grivet-Roux <gmayeul@yahoo.fr>
2018-06-25 11:44:51 +0200

    tp 2

--- src/main/java/TestJpa.java
+++ src/main/java/TestJpa.java
@@ -1,14 +1,35 @@
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import model.Livre;
 
 public class TestJpa {
 
 	public static void main(String[] args) {
+		// initialisation du manager
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_essai");
 		EntityManager em = entityManagerFactory.createEntityManager();
-		
-		System.out.println(em.toString());
+
+		try {
+			// test find()
+			Livre l1 = em.find(Livre.class, 3);
+			if (l1 != null)
+				System.out.println(l1.toString());
+
+			// test TypedQuery
+			TypedQuery<Livre> query = em.createQuery("SELECT l FROM Livre l WHERE l.titre='Germinal'", Livre.class);
+			Livre l2 = query.getResultList().get(0);
+			if (l2 != null)
+				System.out.println(l2.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// fermeture des ressources
+			em.close();
+			entityManagerFactory.close();
+		}
 
 	}
 
---
+++ src/main/java/model/Livre.java
@@ -0,0 +1,65 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="livre")
+public class Livre {
+	@Id
+	private int id;
+	
+	@Column(name="titre")
+	private String titre;
+	
+	@Column(name="auteur")
+	private String auteur;
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the titre
+	 */
+	public String getTitre() {
+		return titre;
+	}
+
+	/**
+	 * @param titre the titre to set
+	 */
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	/**
+	 * @return the auteur
+	 */
+	public String getAuteur() {
+		return auteur;
+	}
+
+	/**
+	 * @param auteur the auteur to set
+	 */
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+	
+	public String toString() {
+		return "[" + this.getId() + "] " + this.getAuteur() + " - " + this.getTitre();
+	}
+}
